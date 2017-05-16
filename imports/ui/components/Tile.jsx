2a03c8ed2672a94801d84eeaeb79648987271ae8
Nelson <nelson.susanto@student.unsw.edu.au>
2017-05-16 18:24:11 +1000

    remove ugly stuff

--- imports/ui/components/Tile.jsx
+++ imports/ui/components/Tile.jsx
@@ -25,9 +25,9 @@ export default class Tile extends Component {
   parseDataIntoGraph(result, news, section){
     if(result != null){
 
-        console.log(result);
-        console.log(news);
-        console.log(section);
+        // console.log(result);
+        // console.log(news);
+        // console.log(section);
   	  var array = result;
   	  var data = [];
   	  var i = 0;
@@ -122,24 +122,7 @@ export default class Tile extends Component {
   	}
   }
 
-	// parseAVDataIntoGraph(result){
-	// 	if(result != null){
-	// 		var array = result;
-	// 		console.log(array);
-	// 		var data = [];
-	// 		var i = 0;
-	// 		while(i < array.length){
-	// 			data.push({
-	// 				name: array[i].Date,
-	// 				value: Math.round((array[i].AV_Return)*100)
-	// 			});
-	// 			i = i +1;
-	// 		}
-  //
-	// 		console.log(data);
-	// 		return data;
-	// 	}
-	// }
+
   //get some info about the company
   getCompanySummary(Name){
   	console.log("the stock name to get summary from is"+ " "+Name);
@@ -156,25 +139,6 @@ export default class Tile extends Component {
   	}
   }
 
-	// parseCMDataIntoGraph(result){
-	// 	if(result != null){
-	// 		var array = result;
-	// 		// console.log(array);
-	// 		var data = [];
-	// 		var i = 0;
-	// 		while(i < array.length){
-	// 			data.push({
-	// 				name: array[i].Date,
-	// 				value: Math.round((array[i].CM_Return)*100)
-	// 			});
-	// 			i = i +1;
-	// 		}
-  //
-	// 		// console.log(data);
-	// 		return data;
-	// 	}
-	// }
-
 
   handleUpdateGraph(eventChange) {
     console.log("Num days for graph will be: " + eventChange.currentTarget.value);
@@ -182,7 +146,6 @@ export default class Tile extends Component {
 
 	//renders a whole bunch of stats for the user
 	render() {
-		// console.log("Tile rendered");
 
     // While we only have functionality for 1 stock
 
@@ -194,16 +157,17 @@ export default class Tile extends Component {
     // console.log("NAME: " + data.name);
     console.log("CODE: " + data.code);
 
-		if(data.code === "Home"){
-		return (
-			<div className="tile">
-				<Item news={"Uber stocks fall amidst scandals"} imagef={"uber.jpg"}/>
-				<Item news={"Apple releases new software"} imagef={"apple.png"}/>
-				<Item news={"ThinkPad designs sleek computer"} imagef={"thinkpad.jpg"}/>
-				<Item news={"UNSW records record numbers"} imagef={"unsw.jpg"}/>
-			</div>
-			);
-		}else{
+    this.parseDataIntoGraph(data.data, news.data, Session.get('sectionNewsData'));
+		if(data.code === "Home") {
+  		return (
+  			<div className="tile">
+  				<Item news={"Uber stocks fall amidst scandals"} imagef={"uber.jpg"}/>
+  				<Item news={"Apple releases new software"} imagef={"apple.png"}/>
+  				<Item news={"ThinkPad designs sleek computer"} imagef={"thinkpad.jpg"}/>
+  				<Item news={"UNSW records record numbers"} imagef={"unsw.jpg"}/>
+  			</div>
+  			);
+		} else {
     	// var companySum = this.getCompanySummary(data.name);
 			// console.log(this.parseDataIntoGraph(data.data));
       var companyReturns = data.data;
@@ -259,37 +223,8 @@ export default class Tile extends Component {
               </tbody>
             </Table>
   					{/*this.getCompanySummary(data.name)*/}
-
 					</div>
-
-          <Form>
-            <GraphButton name={"Last Year"} numDays={365} updateGraph={this.handleUpdateGraph.bind(this)}/>
-            <GraphButton name={"Last Month"} numDays={30} updateGraph={this.handleUpdateGraph.bind(this)}/>
-            <GraphButton name={"Last Week"} numDays={7} updateGraph={this.handleUpdateGraph.bind(this)}/>
-            <GraphButton name={"Today"} numDays={1} updateGraph={this.handleUpdateGraph.bind(this)}/>
-          </Form>
-					<h2>Closing Price</h2>
-
-					<LineChart width={600} height={300} syncId="anyId" data={this.parseDataIntoGraph(data.data, news.data, Session.get('sectionNewsData'))}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-       				<XAxis dataKey="name"/>
-       				<YAxis domain={['auto', 'auto']}/>
-       				<CartesianGrid strokeDasharray="3 3" vertical={false}/>
-       				<Tooltip content={ showTooltipData }/>
-       				<Line type="monotone" dataKey="value" dot={false}  stroke="#8884d8" activeDot={{r: 8}}/>
-      				</LineChart>
-
-              <div id="container"></div>
-					{/*<h2>Cumulative Return</h2>
-					<LineChart width={600} height={300} syncId="anyId" data={this.parseCMDataIntoGraph(data.data)}
-							   margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-						<XAxis dataKey="name"/>
-						<YAxis domain={['auto', 'auto']}/>
-						<CartesianGrid strokeDasharray="3 3" vertical={false}/>
-						<Tooltip content={ showTooltipData }/>
-						<Line type="monotone" dataKey="value" dot={false}  stroke="#8884d8" activeDot={{r: 8}}/>
-					</LineChart>*/}
-
+            <div id="container"></div>
 				</div>
 
 				);
@@ -297,63 +232,29 @@ export default class Tile extends Component {
 	}
 }
 
-function showTooltipData (data) {
-    if ( typeof data.payload[0] !== 'undefined') {
-        // console.log(data.news);
-        // console.log(Object.keys(data.payload[0]));
-        // console.log(Object.values(data.payload[0]));
-    	var date = data.payload[0].payload.name;
-    	var value = data.payload[0].payload.value;
-    	var info = data.payload[0].payload.info;
-    	var url = data.payload[0].payload.url;
-
-      if (typeof(info) != 'undefined') {
-        // return tooltip as url link if there exists an article, or just return share data
-    		return <div id="tag">{date}<br/>
-    							 Price: ${value/100}<br/>
-    			                 <a href={(url !== "") ? url : ""} target="_blank">
-    							 {(info !== "") ? "NEWS:" + info : ""}</a></div>;
-      } else {
-          return <div id="tag">{date}<br/>
-      							 Cumulative Return: {value}%<br/></div>;
-      }
-    }
-
-}
-
-// style={{marginRight: spacing + 'em'}} ,  style="height: 500px; min-width: 310px; max-width: 600px; margin: 0 auto"
-//const CustomTooltip  = React.createClass({
-//     propTypes: {
-//         type: PropTypes.string,
-//         payload: PropTypes.array,
-//         label: PropTypes.string,
-//     },
+// function showTooltipData (data) {
+//     if ( typeof data.payload[0] !== 'undefined') {
+//         // console.log(data.news);
+//         // console.log(Object.keys(data.payload[0]));
+//         // console.log(Object.values(data.payload[0]));
+//     	var date = data.payload[0].payload.name;
+//     	var value = data.payload[0].payload.value;
+//     	var info = data.payload[0].payload.info;
+//     	var url = data.payload[0].payload.url;
 //
-//     getIntroOfPage(label) {
-//         data.news.forEach ( function (news) {
-// 			if (news.date === label) {
-// 				return news.headline;
-// 			}
-// 		})
-//     },
-//
-//     render() {
-//         const { active } = this.props;
-//
-//         if (active) {
-//             const { payload, label } = this.props;
-//             return (
-// 				<div className="custom-tooltip">
-// 					<p className="label">{`${label} : ${payload[0].value}`}</p>
-// 					<p className="intro">{this.getIntroOfPage(label)}</p>
-// 					<p className="desc">Anything you want can be displayed here.</p>
-// 				</div>
-//             );
-//         }
-//
-//         return null;
+//       if (typeof(info) != 'undefined') {
+//         // return tooltip as url link if there exists an article, or just return share data
+//     		return <div id="tag">{date}<br/>
+//     							 Price: ${value/100}<br/>
+//     			                 <a href={(url !== "") ? url : ""} target="_blank">
+//     							 {(info !== "") ? "NEWS:" + info : ""}</a></div>;
+//       } else {
+//           return <div id="tag">{date}<br/>
+//       							 Cumulative Return: {value}%<br/></div>;
+//       }
 //     }
-// });
+//
+// }
 
 
 Tile.propTypes = {
